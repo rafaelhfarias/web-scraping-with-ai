d9569cb32e591da26ed08020f785ec4f770240a5
Rafael Hipólito <rafaelhipolito@Rafaels-MacBook-Pro.local>
2025-02-16 21:01:15 -0300

    feat(main): add max_workers parameter

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ def main():
     parser.add_argument("url", help="Initial URL for crawling")
     parser.add_argument("--keyword", default="Budget", help="keyword to search for")
     parser.add_argument("--max_depth", type=int, default=3, help="Max depth for crawling")
+    parser.add_argument("--max_workers", type=int, default=50, help="Max number of workers for crawling")
     args = parser.parse_args()
 
     db = Database("links.db")
     scraper = WebScraper(db, args.keyword)
-    scraper.crawl(args.url, max_depth=args.max_depth)
+    scraper.crawl(args.url, max_depth=args.max_depth, max_workers=args.workers)
 
 if __name__ == "__main__":
     main()
